5eda03127a6872f53db096af3a6d370a1608e071
Leinerhc <leinerdavidhc20@gmail.com>
2023-03-05 21:50:56 -0500

    main

---
+++ src/Div.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Div(){
+    return  <div className='bg-red-400 h-24 w-24'>v2</div>;
+}
+
+export default Div;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {TagContexto} from './context/ContextoE'
+import Div from './div'
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <TagContexto>
-      <div className='bg-red-400 h-24 w-24'>v2</div>
+      <Div/>
     </TagContexto>
   </React.StrictMode>,
 )
